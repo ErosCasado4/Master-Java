91fcf0a74ba8884bff0fbbf80d713c2afdb150f1
ErosCasado4 <ecl0019@alu.medac.es>
2026-07-09 16:23:41 +0200

    Curso Java

--- HolaMundo/src/EjercicioSentenciaNumeroMenorDiez.java
+++ HolaMundo/src/EjercicioSentenciaNumeroMenorDiez.java
@@ -4,25 +4,26 @@ public class EjercicioSentenciaNumeroMenorDiez {
     public static void main(String[] args) {
 
         int[] numeros = new int[10];
-        int valorMin = numeros[0];
+        int valorMin = Integer.MAX_VALUE;
 
         Scanner s = new Scanner(System.in);
 
         System.out.println("Añade 10 numeros. Veamos cual es el número mas pequeño.");
-        for (int num: numeros) {
-            num = s.nextInt();
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = s.nextInt();
+            int num = numeros[i];
             if (num < 10){
                 System.out.println("El número es menor que 10!");
 
             } else {
                 System.out.println("El numero es igual o mayor que 10!");
             }
-        }
-        for (int i = 0; i < numeros.length; i++) {
             if (numeros[i] < valorMin){
                 valorMin = numeros[i];
             }
         }
-        System.out.println("el numero mas pequeño del array es: " + valorMin);
+             System.out.println("el numero mas pequeño del array es: " + valorMin);
     }
 }
+
